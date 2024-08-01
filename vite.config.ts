45776d2d63fe5d9fc024060e1e507b676ad67a1f
Brandon Roberts <robertsbt@gmail.com>
2024-08-01 13:40:43 -0500

    fix: add test plugin to fix zone.js import issue

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,21 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
+  plugins: [
+    analog(),
+    {
+      name: 'test',
+      transform(code, id, options) {
+        if (options?.ssr && id.includes('zone-node') && code.includes('const global = globalThis')) {
+          console.log(id);
+          return {
+            code: code.replace('const global = globalThis;', '')
+          }
+        }
+          return;
+      },
+    }
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
